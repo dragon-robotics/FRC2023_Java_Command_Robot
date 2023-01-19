b1dd3310162694ed3d23d0a9f2fc1a1cf38f1827
Doug Liu <dougdliu@gmail.com>
2023-01-18 22:47:53 -0700

    1. Added some compressor and double solenoid code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,9 +4,19 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
+  
+  DoubleSolenoid m_doublePCM1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  DoubleSolenoid m_doublePCM2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+
+  Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {}
 
@@ -14,4 +24,32 @@ public class IntakeSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  // Intake pneumatics commands
+  public void pneumaticsExtend() {
+    m_doublePCM1.set(kForward);
+    m_doublePCM2.set(kForward);
+
+  }
+
+  public void pneumaticsRetract() {
+    m_doublePCM1.set(kReverse);
+    m_doublePCM2.set(kReverse);
+
+  }
+
+  public void pneumaticsNeutral() {
+    m_doublePCM1.set(kOff);
+    m_doublePCM2.set(kOff);
+
+  }
+
+  public void compressorOff() {
+    pcmCompressor.disable();
+  }
+
+  public void compressorOn() {
+    pcmCompressor.enableDigital();
+  }
+
 }
